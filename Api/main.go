f4720c141f1122c5bd3412bffad0f76ffa092fac
DiazAdriel0 <diazadriel0@gmail.com>
2023-11-07 17:37:06 -0300

    feat(main): add PUT method

--- Api/main.go
+++ Api/main.go
@@ -24,7 +24,9 @@ var users = []user{
 func main() {
     router := gin.Default()
     router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserByID)
 	router.POST("/users", postUsers)
+	router.PUT("/users/:id", updateUser)
 
     router.Run("localhost:8080")
 }
@@ -46,4 +48,42 @@ func postUsers(c *gin.Context) {
     // Add the new user to the slice.
     users = append(users, newUser)
     c.IndentedJSON(http.StatusCreated, newUser)
+}
+
+// getUserByID locates the user whose ID value matches the id
+// parameter sent by the client, then returns that user as a response.
+func getUserByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of users, looking for
+    // an user whose ID value matches the parameter.
+    for _, a := range users {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+func updateUser(c *gin.Context) {
+	id := c.Param("id")
+	var updatedUser user
+
+    // Call BindJSON to bind the received JSON to newUser.
+    if err := c.BindJSON(&updatedUser); err != nil {
+        return
+    }
+
+
+	// Loop over the list of users, looking for
+	// an user whose ID value matches the parameter.
+	for _, a := range users {
+		if a.ID == id {
+			a = updatedUser
+			c.IndentedJSON(http.StatusOK, updatedUser)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
